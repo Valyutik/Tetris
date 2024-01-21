7c49945fa87f672d7f87f073f13f143c8610480d
Sergei Lind <lind-2004@mail.ru>
2024-01-21 15:06:25 +0300

    Add a shape template

---
+++ Tetris.Unity/Assets/_Project/Develop/PlayForge Team/Tetris/Runtime/Shape.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+namespace PlayForge_Team.Tetris.Runtime
+{
+    public sealed class Shape : MonoBehaviour
+    {
+        public ShapePart[] parts = Array.Empty<ShapePart>();
+    }
+}
---
+++ Tetris.Unity/Assets/_Project/Develop/PlayForge Team/Tetris/Runtime/ShapePart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.Tetris.Runtime
+{
+    public sealed class ShapePart : MonoBehaviour
+    {
+        [SerializeField] private Vector2Int cellId;
+        
+        public void SetPosition(Vector2 position)
+        {
+            transform.position = position;
+        }
+    }
+}
