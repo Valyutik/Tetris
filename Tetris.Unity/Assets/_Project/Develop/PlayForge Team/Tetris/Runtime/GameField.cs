10cc3be14161e5e74d3e4a25b8964c271ac2da37
Sergei Lind <lind-2004@mail.ru>
2024-01-21 14:57:07 +0300

    Add logic to the game field

---
+++ Tetris.Unity/Assets/_Project/Develop/PlayForge Team/Tetris/Runtime/GameField.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.Tetris.Runtime
+{
+    public sealed class GameField : MonoBehaviour
+    {
+        [SerializeField] private Transform firstCellPoint;
+        [SerializeField] private Vector2 cellSize;
+        [SerializeField] private Vector2Int fieldSize;
+        private GameFieldCell[,] _cells;
+        
+        public void FillCellsPositions()
+        {
+            _cells = new GameFieldCell[fieldSize.x, fieldSize.y];
+
+            for (var i = 0; i < fieldSize.x; i++)
+            {
+                for (var j = 0; j < fieldSize.y; j++)
+                {
+                    var cellPosition = (Vector2)firstCellPoint.position + Vector2.right * i * cellSize.x + Vector2.up * j * cellSize.y;
+                    var newCell = new GameFieldCell(cellPosition);
+                    _cells[i, j] = newCell;
+                }
+            }
+        }
+        
+        public Vector2 GetCellPosition(Vector2Int cellId)
+        {
+            var cell = GetCell(cellId.x, cellId.y);
+
+            return cell?.GetPosition() ?? Vector2.zero;
+        }
+        
+        private GameFieldCell GetCell(int x, int y)
+        {
+            if (x < 0 || y < 0 || x >= fieldSize.x || y >= fieldSize.y)
+            {
+                return null;
+            }
+            return _cells[x, y];
+        }
+    }
+}
---
+++ Tetris.Unity/Assets/_Project/Develop/PlayForge Team/Tetris/Runtime/GameFieldCell.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace PlayForge_Team.Tetris.Runtime
+{
+    public sealed class GameFieldCell
+    {
+        private readonly Vector2 _position;
+        private bool _isEmpty;
+        
+        public GameFieldCell(Vector2 position)
+        {
+            _position = position;
+            _isEmpty = true;
+        }
+        
+        public Vector2 GetPosition()
+        {
+            
+            return _position;
+        }
+        
+        public void SetIsEmpty(bool value)
+        {
+            _isEmpty = value;
+        }
+        
+        public bool GetIsEmpty()
+        {
+            return _isEmpty;
+        }
+    }
+}
